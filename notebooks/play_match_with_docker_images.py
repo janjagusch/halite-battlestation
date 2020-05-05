4dc1b13d5beb967d7763eaf525fa5b97f0697e50
jjagusch <jan@instaffo.de>
2020-05-05 20:13:08 +0200

    add match player

--- notebooks/play_match_with_docker_images.py
+++ notebooks/play_match_with_docker_images.py
@@ -13,13 +13,7 @@
 #     name: halite-battlestation
 # ---
 
-from copy import copy
-import socket
-import random
-
-from kaggle_environments import make
-import docker
-import requests
+from cloud_functions_utils import to_topic
 
 # +
 AGENTS = [
@@ -29,56 +23,17 @@ AGENTS = [
     "eu.gcr.io/kaggle-halite/janjagusch/halite-agent:random",
 ]
 
+CONFIGURATION = make("halite").configuration
+
 SEED = 42
 
 TAGS = []
 # -
 
-random.seed(SEED)
-env = make("halite")
-_ = env.reset(num_agents=len(AGENTS))
+messages = [{
+    "agents": AGENTS,
+    "configuration": CONFIGURATION,
+    "seed": i,
+    "tags": TAGS} for i in range(100)]
 
-
-# +
-class _Code:
-    
-    def __init__(self, co_argcount):
-        self.co_argcount = co_argcount
-        
-class RequestAgent:
-    
-    def __init__(self, url):
-        self._url = url
-        self.__code__ = _Code(2)
-        
-    def __call__(self, observation, configuration):
-        data = {"observation": observation, "configuration": configuration}
-        return requests.post(url=self._url, json=data).json()
-
-
-# -
-
-client = docker.from_env()
-
-
-# +
-def free_tcp_port():
-    # https://gist.github.com/gabrielfalcao/20e567e188f588b65ba2
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
-        tcp.bind(('', 0))
-        addr, port = tcp.getsockname()
-        tcp.close()
-        return port
-    
-def host_port(container):
-    # https://github.com/docker/docker-py/issues/1742#issuecomment-329799670
-    return container.attrs["HostConfig"]["PortBindings"]["80/tcp"][0]["HostPort"]
-
-containers = [client.containers.run(agent, detach=True, ports={80: free_tcp_port()}) for i, agent in enumerate(AGENTS)]
-# -
-
-acts = [RequestAgent(f"http://localhost:{host_port(container)}/act") for container in containers]
-
-_ = env.run(copy(acts))
-
-env.render(mode="ipython")
+to_topic(messages, "projects/kaggle-halite/topics/match-request", b64encode=False)
